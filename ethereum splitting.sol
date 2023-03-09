0026cad53baed8159613a7dcfffa6b387363341c
swaroopswamy <126262759+swaroopswamy@users.noreply.github.com>
2023-03-09 12:02:04 +0530

    Create ethereum splitting.sol

--- ethereum splitting.sol
+++ ethereum splitting.sol
@@ -1,11 +1,11 @@
 //0x19CF8B8977b602fd0eD7B4680053c492f40461e8
 
-pragma solidity^0.5.17;
+pragma solidity^0.8.1;
 
 contract ethsplitter {
     function split(address payable user1, address payable user2) external payable {
         require(msg.value % 2 == 0 && msg.value !=0, "value must be even and non-zero");
-        uint splitvalue = msg.value / 2;
+        uint256 splitvalue = msg.value / 2;
         user1.transfer(splitvalue);
         user2.transfer(splitvalue);
     }
